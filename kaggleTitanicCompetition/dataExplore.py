ca42504920b5c4be58fa4c44d9e222208fb78231
Talasila <atalasil@LAMU02Y36T2JG5J.uhc.com>
2019-07-16 16:48:49 -0400

    stuff

--- kaggleTitanicCompetition/dataExplore.py
+++ kaggleTitanicCompetition/dataExplore.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.svm import SVC  
+from sklearn.svm import SVC
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.neural_network import MLPClassifier
+from sklearn import tree
+
 import numpy as np
 
 df = pd.read_csv('train.csv')
@@ -8,12 +12,15 @@ df[['F', 'M']] = pd.get_dummies(df['Sex'])
 df = df.drop(['Name', 'Cabin', 'Ticket', 'Sex', 'Embarked'], axis = 1)
 meanAge = df['Age'].mean()
 df['Age'].fillna(meanAge, inplace = True)
+df[['1', '2', '3']] = pd.get_dummies(df['Pclass'])
+df = df.drop(["Pclass", "PassengerId"], axis=1)
+
 
 trainingInput = df.drop(['Survived'], axis=1)
 trainingOutput = df['Survived']
 
 trainingInput.to_csv('trainingInput.csv')
-svclassifier = SVC(kernel='linear')  
+svclassifier = tree.DecisionTreeClassifier()
 svclassifier.fit(trainingInput, trainingOutput) 
 
 testData = pd.read_csv('test.csv')
@@ -23,11 +30,14 @@ meanAge = testData['Age'].mean()
 meanFare = testData['Fare'].mean()
 testData['Age'].fillna(round(meanAge), inplace = True)
 testData['Fare'].fillna(round(meanFare), inplace = True)
+testData[['1', '2', '3']] = pd.get_dummies(testData['Pclass'])
+PassengerId = testData["PassengerId"]
+testData = testData.drop(["Pclass", "PassengerId"], axis=1)
 testData.to_csv('modifiedTestData.csv')
 testingOutput = svclassifier.predict(testData)
 
-outPutData = testData['PassengerId']
-survived = pd.Dataframe(testingOutput)
+outPutData = pd.DataFrame(PassengerId)
+survived = pd.DataFrame(testingOutput)
 
-survived.to_csv('survived.csv')
+outPutData["Survived"] = survived
 outPutData.to_csv('predictions.csv')
